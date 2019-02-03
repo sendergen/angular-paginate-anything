78f235d3400e6f74f8b5781995266ed3e82aa473
Lola <lola@Sherees-MacBook-Air-2.local>
2019-02-02 21:47:25 -0800

    Added ability to style the pager

--- dist/paginate-anything.js
+++ dist/paginate-anything.js
@@ -71,6 +71,8 @@
           linkGroupSize: '=?',
           reloadPage: '=?',
           size: '=?',
+          selectClass: '=?',
+          perPageClass: '=?',
           passive: '@',
           transformResponse: '=?',
           method: '@',
